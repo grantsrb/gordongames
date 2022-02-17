d3244fab3767f56e93dc0b3b816080420ee70ca1
Satchel Grant <grantsrb@gmail.com>
2022-02-17 15:00:02 -0800

    minor changes to testing

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -4,33 +4,40 @@ import gym
 
 if __name__=="__main__":
     args = {
-        "grid_size": (9,9),
-        "pixel_density": 1,
-        "targ_range": (8,8),
+        "grid_size": (11,11),
+        "pixel_density": 3,
+        "targ_range": (1,8),
         "harsh": False,
     }
-    env = gym.make("gordongames-v4", **args)
+    env_types = [
+        "gordongames-v4",
+        "gordongames-v0",
+        "gordongames-v1",
+    ]
+    for env_type in env_types:
+        print("making new env", env_type)
+        env = gym.make(env_type, **args)
 
-    done = False
-    rew = 0
-    obs = env.reset()
-    env.render()
-    key = ""
-    action = "w"
-    while key != "q":
-        key = input("action: ")
-        if key   == "w": action = UP
-        elif key == "d": action = RIGHT
-        elif key == "s": action = DOWN
-        elif key == "a": action = LEFT
-        elif key == "f": action = 5
-        else: action = STAY
-        obs, rew, done, info = env.step(action)
-        print("rew:", rew)
-        print("done:", done)
-        print("info")
-        for key in info.keys():
-            print("    ", key, ": ", info[key])
-        if done:
-            obs = env.reset()
+        done = False
+        rew = 0
+        obs = env.reset()
         env.render()
+        key = ""
+        action = "w"
+        while key != "q":
+            key = input("action: ")
+            if key   == "w": action = UP
+            elif key == "d": action = RIGHT
+            elif key == "s": action = DOWN
+            elif key == "a": action = LEFT
+            elif key == "f": action = 5
+            else: action = STAY
+            obs, rew, done, info = env.step(action)
+            print("rew:", rew)
+            print("done:", done)
+            print("info")
+            for k in info.keys():
+                print("    ", k, ":", info[k])
+            if done:
+                obs = env.reset()
+            env.render()
